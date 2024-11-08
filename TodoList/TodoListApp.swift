4c0d66ca3b69cdf436377f7f53aaccf7fa9cc3a9
audy21 <audymuhammad@gmail.com>
2024-11-08 14:37:19 +0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,24 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Audy M. on 06/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    
+    @StateObject var listViewModel: ListViewModel = ListViewModel()
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ListView()
+            }
+            .navigationViewStyle(StackNavigationViewStyle())
+            .environmentObject(listViewModel)
+        }
+    }
+}
